7a8ed607ae3fbba9e64dac691581975bc69cb0f1
Michal <mbularz95@interia.pl>
2018-05-10 21:44:31 +0200

    style css layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,13 +22,13 @@ class Main extends React.Component {
   render() {
     return (
       <Router>
-      <div className='container-fluid'>
+      <div className='container-fluid px-0'>
         <TopBar />
-        <div className='row mb-4'>
-          <div className='col-lg-2'>
+        <div className='row mx-0'>
+          <div className='col-lg-2 px-0'>
             <Sidebar />
           </div>
-          <div className='col-lg-10'>
+          <div className='col-lg-10 mb-4 pl-4'>
             <Route exact path="/" component={Cocpit} />
             <Route path="/addPage" component={AddPage} />
             <Route path="/stats" component={Stats} />
