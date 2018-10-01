3d516c3ac7502a4074e9885e9d9595b5188072e4
undercover95 <mbularz95@interia.pl>
2018-10-01 21:43:28 +0200

    Remove server & dist

--- server/startBackend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Starting mysql server..."
-
-echo $(docker run -d --rm --name mysql --rm -e MYSQL_ROOT_PASSWORD=1995 -e MYSQL_DATABASE=page_monitor_db -e MYSQL_USER=admin -e MYSQL_PASSWORD=1995 -e TZ='Europe/Warsaw' monitor-stron-mysql)
-
-echo "Starting apache+php server..."
-echo $(docker run -d -p 80:80 --rm --name backend --link mysql:mysql -v "$PWD":/var/www/html monitor-stron-backend /usr/sbin/apache2ctl -D FOREGROUND)
-
-echo "Done."
